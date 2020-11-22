d1477ecb83ab08336b216d9d74035f35d233eb2f
siddharth <pandeysiddharth03@gmail.com>
2020-11-22 09:59:02 +0530

    cors

--- src/main/groovy/com/brainup/readby/ReadbyApplication.groovy
+++ src/main/groovy/com/brainup/readby/ReadbyApplication.groovy
@@ -3,21 +3,17 @@ package com.brainup.readby
 import com.brainup.readby.security.JWTAuthorizationFilter
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.http.HttpMethod
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 class ReadbyApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(ReadbyApplication, args)
 	}
 
@@ -27,22 +23,11 @@ class ReadbyApplication {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
+			http.cors().and().csrf().disable()
 					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
 					.authorizeRequests()
 					.antMatchers(HttpMethod.POST, "/user").permitAll()
 					.anyRequest().authenticated();
-		}
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
-				//registry.addMapping("/readBy").allowedOrigins("*");
-			}
 		}
 	}
 }
--- src/main/groovy/com/brainup/readby/controller/ReadByAdminController.groovy
+++ src/main/groovy/com/brainup/readby/controller/ReadByAdminController.groovy
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
-//@CrossOrigin(origins = "*")
 @RequestMapping(value = "/readBy/admin")
 @Slf4j
 class ReadByAdminController {
